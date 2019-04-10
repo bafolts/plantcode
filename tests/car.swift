2b5c1d8abcfd57a83c365212227fa617e457a367
kogepan159 <kogepan6852@yahoo.co.jp>
2019-04-10 13:51:41 +0900

    swift is Beta support

---
+++ tests/car.swift
@@ -0,0 +1,62 @@
+class Car {
+  var model: String 
+  var make: String 
+  var year: Number 
+  func setModel(model:String) {
+  }
+  func setMake(make:String) {
+  }
+  func setYear(param0) {
+  }
+  func getModel() {
+  }
+  func getMake() {
+  }
+  func getYear() {
+  }
+}
+
+
+
+
+
+
+
+class NamesInThings {
+  var field: String 
+  var field1: String1 
+  var _some_private: String 
+  var field_2: String_2 
+  func member() {
+  }
+  func member2() {
+  }
+  func member3() {
+  }
+  func member_s() {
+  }
+}
+
+
+
+class Toyota : Car {
+}
+
+
+
+class Honda : Car {
+}
+
+
+
+class Ford : Car {
+}
+
+
+
+class Hyundai : Car {
+}
+
+
+
+
