ee56ee6f5915063baf12280056caae8cc9198beb
kogepan159 <kogepan6852@yahoo.co.jp>
2019-04-17 12:32:18 +0900

    sReturnType Check  maked method.

--- tests/car.swift
+++ tests/car.swift
@@ -1,18 +1,24 @@
 class Car {
   var model: String?  = nil;
   var make: String?  = nil;
-  var year: Int?  = nil;
-  func setModel(model:String) {
+  var year: Number?  = nil;
+  func setModel(model:String){
+    
   }
-  func setMake(make:String) {
+  func setMake(make:String){
+    
   }
-  func setYear() {
+  func setYear(param0){
+    
   }
-  func getModel() {
+  func getModel() -> String{
+     return String()
   }
-  func getMake() {
+  func getMake() -> String{
+     return String()
   }
-  func getYear() {
+  func getYear() -> Number{
+     return Number()
   }
 }
 
@@ -24,16 +30,52 @@ class Car {
 
 class NamesInThings {
   var field: String?  = nil;
-  var field1: String?  = nil;
+  var field1: String1?  = nil;
   var _some_private: String?  = nil;
-  var field_2: String?  = nil;
-  func member() {
+  var field_2: String_2?  = nil;
+  func member(){
+    
   }
-  func member2() {
+  func member2() -> String1{
+     return String1()
   }
-  func member3() {
+  func member3(){
+    
   }
-  func member_s() {
+  func member_s() -> String2{
+     return String2()
+  }
+}
+
+
+
+class Roll {
+  var id: Int?  = nil;
+  var typename: String?  = nil;
+  var type: Int?  = nil;
+  func commentDraft(){
+    
+  }
+  func commentPost(){
+    
+  }
+  func commentGet(){
+    
+  }
+  func commentDelete(){
+    
+  }
+  func questDraft(){
+    
+  }
+  func questPost(){
+    
+  }
+  func questGet(){
+    
+  }
+  func questDelete(){
+    
   }
 }
 
